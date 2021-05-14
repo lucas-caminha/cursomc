8d1e51639ad9493ba92d750fcb92ad26d0619de8
lucas.caminha <lucas.caminha@softplan.com.br>
2021-05-14 09:27:07 -0300

    Disponibilizado delete de categorias

--- src/main/java/br/com/lucas/cursospring/services/CategoriaService.java
+++ src/main/java/br/com/lucas/cursospring/services/CategoriaService.java
@@ -3,10 +3,12 @@ package br.com.lucas.cursospring.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import br.com.lucas.cursospring.domain.Categoria;
 import br.com.lucas.cursospring.repository.CategoriaRepository;
+import br.com.lucas.cursospring.services.exceptions.DataIntegrityException;
 import br.com.lucas.cursospring.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -32,6 +34,15 @@ public class CategoriaService {
 	public Categoria update(Categoria categoria) {	
 		find(categoria.getId());
 		return categoriaRepository.save(categoria);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			categoriaRepository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos.");
+		}
 	}
 	
 }
