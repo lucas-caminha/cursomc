dc2e83b20aaccebe1f39dc0e09b5eff1b41946f3
lucas.caminha <lucas.caminha@softplan.com.br>
2021-05-14 13:43:35 -0300

    Get, Put e Delete para Cliente

---
+++ src/main/java/br/com/lucas/cursospring/dto/ClienteDTO.java
@@ -0,0 +1,56 @@
+package br.com.lucas.cursospring.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import br.com.lucas.cursospring.domain.Cliente;
+
+public class ClienteDTO implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	@Size(min = 5, max = 120, message = "O Tamanho deve estar entre 5 e 80 caracteres.")
+	@NotNull(message = "Preenchimento obrigatório")
+	private String nome;
+	@NotNull
+	@Email(message = "E-mail inválido.")
+	private String email;
+	
+	public ClienteDTO() {
+	}
+	
+	public ClienteDTO(Cliente cliente) {
+		this.id = cliente.getId();
+		this.nome = cliente.getNome();
+		this.email = cliente.getEmail();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
--- src/main/java/br/com/lucas/cursospring/services/ClienteService.java
+++ src/main/java/br/com/lucas/cursospring/services/ClienteService.java
@@ -1,12 +1,19 @@
 package br.com.lucas.cursospring.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.lucas.cursospring.domain.Cliente;
+import br.com.lucas.cursospring.dto.ClienteDTO;
 import br.com.lucas.cursospring.repository.ClienteRepository;
+import br.com.lucas.cursospring.services.exceptions.DataIntegrityException;
 import br.com.lucas.cursospring.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -23,4 +30,42 @@ public class ClienteService {
 		return cliente;
 	}
 	
+	public Cliente insert(Cliente cliente) {
+		cliente.setId(null);
+		return clienteRepository.save(cliente);
+	}
+
+	public Cliente update(Cliente cliente) {	
+		Optional<Cliente> newCli = find(cliente.getId());
+		updateData(newCli.get(), cliente);
+		return clienteRepository.save(newCli.get());
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			clienteRepository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir um Cliente porque há Entidades relacionadas.");
+		}
+	}
+
+	public List<Cliente> findAll() {
+		return clienteRepository.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return clienteRepository.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO cliDto) {
+		return new Cliente(cliDto.getId(), cliDto.getNome(), cliDto.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newCli, Cliente cliente) {
+		newCli.setNome(cliente.getNome());
+		newCli.setEmail(cliente.getEmail());
+		
+	}
 }
