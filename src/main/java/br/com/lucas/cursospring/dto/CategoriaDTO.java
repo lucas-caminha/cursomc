6b104bf186cfc8719f86df22c891dd87d1810188
lucas.caminha <lucas.caminha@softplan.com.br>
2021-05-14 09:44:22 -0300

    Disponibilizado findall de categorias

---
+++ src/main/java/br/com/lucas/cursospring/dto/CategoriaDTO.java
@@ -0,0 +1,38 @@
+package br.com.lucas.cursospring.dto;
+
+import java.io.Serializable;
+
+import br.com.lucas.cursospring.domain.Categoria;
+
+public class CategoriaDTO implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String nome;
+	
+	public CategoriaDTO() {
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public CategoriaDTO(Categoria categoria) {
+		this.id = categoria.getId();
+		this.nome = categoria.getNome();
+	}
+
+}
--- src/main/java/br/com/lucas/cursospring/resources/CategoriaResource.java
+++ src/main/java/br/com/lucas/cursospring/resources/CategoriaResource.java
@@ -1,7 +1,9 @@
 package br.com.lucas.cursospring.resources;
 
 import java.net.URI;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.lucas.cursospring.domain.Categoria;
+import br.com.lucas.cursospring.dto.CategoriaDTO;
 import br.com.lucas.cursospring.services.CategoriaService;
 
 @RestController
@@ -49,5 +52,13 @@ public class CategoriaResource {
 		categoriaService.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {	
+		List<Categoria> categorias = categoriaService.findAll();
+		List<CategoriaDTO> categoriasDTO = categorias.stream().map(cat -> new CategoriaDTO(cat)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(categoriasDTO);
+	}
+	
 	
 }
